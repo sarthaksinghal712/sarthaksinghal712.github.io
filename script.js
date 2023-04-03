c712cb61e705e7d575029478b1cfb86330884d9f
sarthaksinghal712 <sarthaksinghal712@gmail.com>
2023-04-03 23:04:02 +0800

    updated with project 2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $('.scroll').click(function(event) {
-    $("#content1").slideDown("fast")
+    // $("#content1, #content2").slideDown("fast")
     event.preventDefault();
     var targetOffset = $('#content1').offset().top;
     $("html, body").animate({
@@ -9,10 +9,21 @@ $('.scroll').click(function(event) {
 
 $("#knowProj1").click(function(event) {
     event.preventDefault();
-    $("#content-card").css("height", $("#card-intro").height())
-    $("#frame").css("width", $("#card-intro").width() - 30)
-    $("#content-card").slideDown("slow");
-    var targetOffset = $('#content-card-footer').offset().top;
+    $("#content-card1").css("height", $("#card-intro1").height())
+    $("#frame").css("width", $("#card-intro1").width() - 30)
+    $("#content-card1").slideDown("slow");
+    var targetOffset = $('#content-card-footer1').offset().top;
+    $("html, body").animate({
+        scrollTop: targetOffset + "px"
+    },500);
+})
+
+$("#knowProj2").click(function(event) {
+    event.preventDefault();
+    $("#content-card2").css("height", $("#card-intro2").height())
+    $("#frame2").css("width", $("#card-intro2").width() - 30)
+    $("#content-card2").slideDown("slow");
+    var targetOffset = $('#content-card-footer2').offset().top;
     $("html, body").animate({
         scrollTop: targetOffset + "px"
     },500);
